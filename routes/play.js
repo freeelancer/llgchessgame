53c4da91d9c04b0b90026eca3a4dd4d86ded6421
freeelancer <lmengsplist96@gmail.com>
2024-05-15 01:01:28 +0800

    simple integration for contract

--- routes/play.js
+++ routes/play.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var util = require('../config/util.js');
+const { ethers } = require('ethers');
+const { LLG } = require('../abis/llg.js');
 var router = express.Router();
 
 router.get('/', function(req, res) {
@@ -16,5 +18,30 @@ router.post('/', function(req, res) {
     var token = util.randomString(20);
     res.redirect('/game/' + token + '/' + side);
 });
+
+let contract = getContract();
+
+router.get('/test', async function(req, res) {
+    if (!contract) {
+        contract = getContract();
+    }
+    const decimals = await contract.decimals();
+    res.send(decimals)
+});
+
+function getContract() {
+    if (!process.env.RPC_URL || !process.env.CONTRACT) {
+        throw new Error('RPC_URL and CONTRACT must be set in the environment variables');
+    }
+    const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+    if (!provider) {
+        throw new Error('Provider not found');
+    }
+    if (!process.env.CONTRACT) {
+        throw new Error('Contract not found');
+    }
+    const contract = new ethers.Contract(process.env.CONTRACT, LLG, provider);
+    return contract;
+}
 
 module.exports = router;
